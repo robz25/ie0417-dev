dc430997547987a886a3b1e4d62ba8a008123a48
Arkantos <gualixgo@gmail.com>
2022-06-11 20:19:32 -0600

    testing

---
+++ labs/lab3/eie_device_server/build/CMakeFiles/deploy_tmp_sysroot.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deploy_tmp_sysroot"
+  "tmp_sysroot"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/deploy_tmp_sysroot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ labs/lab3/eie_device_server/build/CMakeFiles/sensor_commands.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sensor_commands.dir/src/command.c.o"
+  "CMakeFiles/sensor_commands.dir/src/command_runner.c.o"
+  "CMakeFiles/sensor_commands.dir/src/sensor/analyzers/avg_thresh.c.o"
+  "CMakeFiles/sensor_commands.dir/src/sensor/manager.c.o"
+  "CMakeFiles/sensor_commands.dir/src/sensor/sensor.c.o"
+  "CMakeFiles/sensor_commands.dir/src/sensor/sensors/factory.c.o"
+  "CMakeFiles/sensor_commands.dir/src/sensor/sensors/level.c.o"
+  "CMakeFiles/sensor_commands.dir/src/sensor/sensors/temp.c.o"
+  "CMakeFiles/sensor_commands.dir/src/version.c.o"
+  "libsensor_commands.pdb"
+  "libsensor_commands.so"
+  "libsensor_commands.so.0.0.1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sensor_commands.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
