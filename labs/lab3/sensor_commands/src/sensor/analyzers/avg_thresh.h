ff86a71ff5c2960ab313a6646cc86d942aa557f1
kathalsol <alfarosolisk@gmail.com>
2022-06-13 08:55:33 -0600

    Deleting examples of the class from the lab

--- labs/lab3/sensor_commands/src/sensor/analyzers/avg_thresh.h
+++
@@ -1,6 +0,0 @@
-#ifndef AVG_THRESH_H_
-#define AVG_THRESH_H_
-
-
-
-#endif // AVG_THRESH_H_
--- labs/lab3/sensor_commands/src/sensor/sensors/temp.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEMP_H_
-#define TEMP_H_
-
-#include <sensor_commands/sensor/sensor.h>
-
-struct Sensor * temp_sensor_create(const char *name);
-
-#endif // TEMP_H_
