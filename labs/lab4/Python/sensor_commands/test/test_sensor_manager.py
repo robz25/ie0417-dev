0838bb72e36a29ba00ddb81882e1f9416cd98d3f
German A. Ureña Araya <gualixgo@gmail.com>
2022-06-20 20:38:34 -0600

    adding new functions

---
+++ labs/lab4/Python/sensor_commands/test/test_sensor_manager.py
@@ -0,0 +1,27 @@
+
+from distutils.command.config import config
+import pytest
+
+from sensor_commands.sensor.manager import SensorManager
+import demo_api
+
+@fixture
+def sensor_mgr():
+    config_name = "../config/sensors_cfg.json" #test test se debe quitar los ..
+    sensor_man = SensorManager(config_name)
+    return sensor_man
+
+def test_sensor_manager_supported_types(sensor_mgr):
+
+    pass
+
+
+
+def test_sensor_manager_supported_types(sensor_mgr):
+
+    pass
+
+def test_demo_api_mult_op():
+    r = demo_api.mult(5, 4)
+    logging.info(f"Mult result: {r}")
+    assert r == 20, "Multiplication failed!"
