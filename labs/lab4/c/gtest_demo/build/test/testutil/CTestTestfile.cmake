0caacaa8bb1e305a3888ef8972b86fa29660ea4a
kathalsol <alfarosolisk@gmail.com>
2022-06-30 20:56:58 -0600

    Merge parte python de Robin

--- labs/lab4/c/gtest_demo/build/test/testutil/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dev/ws/labs/lab4/c/gtest_demo/test/testutil
-# Build directory: /home/dev/ws/labs/lab4/c/gtest_demo/build/test/testutil
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
